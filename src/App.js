6431305cbc8effa3a2a554be0a20800ffe54a858
Amala Sebastian <107768312+Amalascrobits@users.noreply.github.com>
2022-12-01 17:47:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import Qrcode from 'qrcode.react'
+import React from 'react'
+import { useEffect } from 'react';
+import Tilt from './pages/Tilt';
+// import './form.css'
+
+const App=()=> {
+
+ 
+
+  return (
+  <>
+  <div>
+
+    <Tilt/>
+    {/* <Self/> */}
+
+  </div>
+
+  </>
+  );
+}
+
+export default App;
---
+++ src/pages/Tilt.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../style/tilt.css'
+
+const Tilt = () => {
+  return (
+<>
+<div className="container">
+	<div className="tilt-box-wrap">
+		<span className="t_over"></span>
+		<span className="t_over"></span>
+		<span className="t_over"></span>
+		<span className="t_over"></span>
+		<span className="t_over"></span>
+		<span className="t_over"></span>
+		<span className="t_over"></span>
+		<span className="t_over"></span>
+		<span className="t_over"></span>
+		<div className="tilt-box">
+			<strong>Tilt <br/> Effect</strong>
+		</div>
+	</div>
+</div>
+</>
+  )
+}
+
+export default Tilt
